87ef64ef07a053ae0983cdc34435dd743ea3b675
user <user@DESKTOP-GG15ICR>
2018-09-26 22:30:24 +0530

    commit2

--- src/com/actitime/generics/ExcelDemo.java
+++ src/com/actitime/generics/ExcelDemo.java
@@ -5,6 +5,7 @@ public class ExcelDemo
 
 	public static void main(String[] args)
 	{
+		System.out.println("Haii....");
 		String data=ExcelData.getData("./data/input.xlsx","Sheet1", 0, 0);
         System.out.println(data);
 		int rc=ExcelData.getRowCount("./data/input.xlsx","Sheet1");
